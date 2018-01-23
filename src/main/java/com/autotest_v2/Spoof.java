7d0579326e14fbdc6fd0c6fec14e4235279410e4
OleksandrSotnikov <alex_qa@yahoo.com>
2018-01-23 12:32:54 +0200

    Short xpath for spoofing.

--- src/main/java/com/autotest_v2/Spoof.java
+++ src/main/java/com/autotest_v2/Spoof.java
@@ -2,25 +2,22 @@ package com.autotest_v2;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.util.concurrent.TimeUnit;
 
 public class Spoof {
-    public static void spoof(WebDriver cd, String countrycode, String postalcode) {
+    public static void spoof(WebDriver cd, String countryCode, String postalCode) {
 
-        By SpoofCountryInput = By.xpath("//*[@id=\"root\"]/div/div/div[2]/div[1]/div/form/div/div[2]/div[1]/input");
-        By SpoofZipInput = By.xpath("//*[@id=\"root\"]/div/div/div[2]/div[1]/div/form/div/div[2]/div[2]/input");
-        By SpoofButton = By.xpath("//*[@id=\"root\"]/div/div/div[2]/div[1]/div/form/div/div[3]/button[1]/div");
+        By SpoofCountryInput = By.xpath("(//input)[1]");
+        By SpoofZipInput = By.xpath("(//input)[2]");
+        By SpoofButton = By.xpath("(//button)[@type=\"submit\"]");
 
-        cd.findElement(SpoofCountryInput).sendKeys(countrycode);
+        cd.findElement(SpoofCountryInput).sendKeys(countryCode);
 
-        cd.findElement(SpoofZipInput).sendKeys(postalcode);
+        cd.findElement(SpoofZipInput).sendKeys(postalCode);
 
         cd.findElement(SpoofButton).click();
     }
 
-    public static void main(String[] args) {
+/*    public static void main(String[] args) {
 
         // for Chrome.
         System.setProperty("webdriver.chrome.driver", "D:/ProgramData/SeleniumStuff/chromedriver.exe");
@@ -38,5 +35,5 @@ public class Spoof {
         cd.get("https://qa.fubo.tv/sandbox/geolocation");
 
         spoof(cd, "USA", "94124");
-    }
+    }*/
 }
