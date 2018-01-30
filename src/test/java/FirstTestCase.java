1576587090c83049dde6841876bedd3a52416d9e
OleksandrSotnikov <alex_qa@yahoo.com>
2018-01-30 11:11:27 +0200

    Web Driver wait instead of sleep.

--- src/test/java/FirstTestCase.java
+++ src/test/java/FirstTestCase.java
@@ -1,8 +1,11 @@
 import com.autotest_v2.PageObject.WelcomePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -10,11 +13,13 @@ import java.util.concurrent.TimeUnit;
 
 public class FirstTestCase {
     @Test
-    public void welcomePage() throws InterruptedException {
+    public void welcomePage()  {
         System.setProperty("webdriver.chrome.driver", "D:/ProgramData/SeleniumStuff/chromedriver.exe");
 
         // Open browser.
         WebDriver driver = new ChromeDriver();
+
+        By channelsButton = By.xpath("(//a[@href=\"/welcome/channels\"]/span[.=\"Channels\"])[2]");
 
         // Set timeout.
         driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
@@ -34,7 +39,9 @@ public class FirstTestCase {
 
         driver.findElement(spoofButton).click();
 
-        Thread.sleep(3000);
+        WebDriverWait wait = new WebDriverWait(driver,60);
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(channelsButton));
 
         WelcomePage welcomePage = PageFactory.initElements(driver, WelcomePage.class);
 
